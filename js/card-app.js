3f3522a7318c8aae2cef3f061217349388cbc127
Matthew <herrigesmt@gmail.com>
2020-06-22 13:41:56 -0700

    added function names for card-app.js

--- js/card-app.js
+++ js/card-app.js
@@ -1,5 +1,55 @@
 'use strict';
 
+// constructor function
+
+function Card(imgSrc, cardName, blurb, description){
+  this.imgSrc = imgSrc;
+  this.cardName = cardName;
+  this.blurb = blurb;
+  this.description = description;
+
+  Card.collection.push(this);
+}
+
+Card.collection = [];
+
+// declare functions for displaying cards
 
 
+
+// randomizer function
+
+function randomizer(){
+
+}
+
+// use randomizer to pick images
+
+function pickCard(){
+// use randomizer function in here
+}
+
+
+// render cards to web page
+
+function render(){
+
+}
+
+// event handler function
+
+function storeLocal(){
+
+}
+// event listener for submit
+
+var submit = document.getElementById('user-input');
+submit.addEventListener('submit', handleEvent);
+
+function handleEvent(event){
+  event.preventDefault();
+  pickCard();
+  storeLocal();
+  render();
+}
 
